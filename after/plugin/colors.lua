c61987ed1ef111540771d3ba099068f6e3995b57
Azaan <azaanwadhwania@yahoo.co.in>
2023-10-19 10:07:15 -0700

    change comment style

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,9 +3,14 @@ require("tokyonight").setup({
     style = "storm",
     styles = {
         sidebars = "dark",
-        comments = {italic = true}
+        comments = {italic = true},
     },
-    sidebars = { "qf", "help" }
+    sidebars = { "qf", "help" },
+    on_colors = function (colors)
+        --comment colour
+        colors.comment = "#5acc33"
+        colors.error = "#ff0000"
+    end
 })
 
 vim.cmd[[colorscheme tokyonight]]
